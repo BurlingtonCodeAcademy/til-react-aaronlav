689b65887123356f4b0d28cd79e29fad5107fc85
Aaron Lavery <aaroncl55@gmail.com>
2019-08-28 16:21:25 -0400

    new push

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Today I Learned</h1>
-      <li><a href='/facts'>List all entries (JSON)</a></li>
-      <h2>Add a fact</h2>
-      <form method="POST" action="/facts">
-        <input type="text" name="text"></input>
-        <input type="submit"></input>
-      </form>
+        <li><a href='/facts'>List all entries (JSON)</a></li>
+        <h2>Add a fact</h2>
+        <form method="POST" action="/facts">
+          <input type="text" name="text"></input>
+          <input type="submit"></input>
+        </form>
       </header>
     </div>
   );
--- til_web.js
+++ til_web.js
@@ -1,17 +1,36 @@
-/*
-  Today I Learned webapp
-*/
 const assert = require('assert');
 const FactStore = require('./lib/factStore')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const path = require('path')
+const static = path.join(__dirname, "static")
 
 app.use(express.static('static')) // static file server
-app.use(express.urlencoded({extended: true})) // all POST bodies are expected to be URL-encoded
+app.use(express.urlencoded({ extended: true })) // all POST bodies are expected to be URL-encoded
 
 const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const store = new FactStore(dbUrl);
+
+app.get('/facts/:objectId', getOne)
+
+async function getOne(request, response) {
+  let facts = await store.get(request.params.objectId)
+  let output = [];
+  facts.forEach((entry) => {
+    output.push(entry);
+  }, function (err) {
+    assert.equal(null, err);
+    console.log("Sending " + output.length + " records to client");
+    response.type('application/json')
+      .send(JSON.stringify(output))
+  });
+}
+
+
+app.get('/factList', function (request, response) {
+  response.sendFile(path.join(static, 'facts.html'))
+});
 
 app.get('/facts', getAll);
 
@@ -28,6 +47,10 @@ async function getAll(request, response) {
   });
 }
 
+app.get('/:fact', function (request, response) {
+  response.sendFile(path.join(static, 'fact.html'))
+});
+
 app.post('/facts', addFact);
 
 async function addFact(request, response) {
@@ -41,14 +64,23 @@ async function addFact(request, response) {
     .send(JSON.stringify(output))
 }
 
-app.delete('delete fact', deleteFact)
-async () => {
-  const result = await App.deleteFact(request.body.text.delete())
-  expect(result).toBe(1);
-
-  await App.deleteOne({id: result.id})
-  const newResult = await App.deleteFact();
-  expect(fact).toBe(0);
+app.post('/deleted', deleteFact);
+async function deleteFact(request, response) {
+  let result = await store.deleteFact(request.body.text.trim())
+  let output = {
+    status: 'ok',
+    id: result.id
+  }
 }
+
+// app.delete('delete fact', deleteFact)
+// async () => {
+//   const result = await App.deleteFact(request.body.text.delete())
+//   expect(result).toBe(1);
+
+//   await App.deleteOne({id: result.id})
+//   const newResult = await App.deleteFact();
+//   expect(fact).toBe(0);
+// }
 
 app.listen(port, () => console.log(`TIL web app listening on port ${port}!`))
